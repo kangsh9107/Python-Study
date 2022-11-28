47fcc9687ee2b60eba225a84e94eafb79259046f
kangsh9107 <kangsh9107@naver.com>
2022-11-28 18:27:45 +0900

    study

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
-print("\"Hello\\nWorld\"")
+# a = 1
+# b = 2
+# print(a+b)
+
+# str = "Python"
+# print(str)
+
+# if a > 1:
+#     print(a)
+# else:
+#     print(b)
+
+# for c in [1,2,3,4,5]:
+#     print(c)
+
+# i = 0
+# while i < 3:
+#     i = i + 1
+#     print(i)
+
+# def add(x, y):
+#     return x + y
+
+# print(add(3, 4))
+
+"""
+주석주석주석
+"""
+
