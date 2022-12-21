fc62746ff80ef36663e130b72b59e0621ab1fed0
kangsh9107 <kangsh9107@naver.com>
2022-12-21 17:25:08 +0900

    2022-12-20~2022-12-21

---
+++ class/2022-12-21.py
@@ -0,0 +1,19 @@
+# a = 'i eat 100 apples.'
+# print(a)
+
+# number = 1
+# str = 'i eat %d apples.' %(number)
+# print(str)
+
+# a = [1,2,3,4,5]
+# print(a[1:2])
+
+# a = "hobby"
+# print(a.count("B"))
+
+# a = "Python is the best choice"
+# print(a.find('z'))
+
+a = "abcd"
+print(",".join(a))
+
