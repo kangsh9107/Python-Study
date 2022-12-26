3e784aa45360a5144d6d1e188fb48b813934c3c9
kangsh9107 <kangsh9107@naver.com>
2022-12-26 11:57:22 +0900

    study

---
+++ class/2022-12-26.py
@@ -0,0 +1,13 @@
+# def GuGu(n):
+#     result = []
+#     i = 1
+#     while i < 10:
+#         result.append(n * i)
+#         i += 1
+#     return result
+# print(GuGu(2))
+
+a = "a:b:c:d"
+print(a)
+print("#".join(a.split(":")))
+
